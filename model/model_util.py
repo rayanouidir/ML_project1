f1b436248320b8b8114252cd544573daeb50bd1b
root <root@Rayan>
2026-09-23 18:16:59 +0200

    add model

---
+++ model/model_util.py
@@ -0,0 +1,204 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import datetime
+
+
+def compute_loss(y, tx, w):
+    """Calculate the loss using either MSE or MAE.
+
+    Args:
+        y: numpy array of shape=(N, )
+        tx: numpy array of shape=(N,2)
+        w: numpy array of shape=(2,). The vector of model parameters.
+
+    Returns:
+        the value of the loss (a scalar), corresponding to the input parameters w.
+    """
+    # ***************************************************
+    # INSERT YOUR CODE HERE
+    # TODO: compute loss by MSE
+    # ***************************************************
+    n = y.shape[0]
+    e = y - tx.dot(w)
+    mse = (1/2*n)* np.sum(e ** 2)
+    return mse
+
+
+def compute_gradient(y, tx, w):
+    """Computes the gradient at w.
+
+    Args:
+        y: numpy array of shape=(N, )
+        tx: numpy array of shape=(N,2)
+        w: numpy array of shape=(2, ). The vector of model parameters.
+
+    Returns:
+        An numpy array of shape (2, ) (same shape as w), containing the gradient of the loss at w.
+    """
+    # ***************************************************
+    # INSERT YOUR CODE HERE
+    # TODO: compute gradient vector
+    # ***************************************************
+    e = y - tx.dot(w)          
+    N = y.shape[0]
+    grad = -1/N* np.dot(np.transpose(tx),e)
+    
+    return grad
+
+
+
+def mean_squared_error_gd(y, tx, initial_w, max_iters, gamma):
+    """The Gradient Descent (GD) algorithm.
+
+    Args:
+        y: numpy array of shape=(N, )
+        tx: numpy array of shape=(N,2)
+        initial_w: numpy array of shape=(2, ). The initial guess (or the initialization) for the model parameters
+        max_iters: a scalar denoting the total number of iterations of GD
+        gamma: a scalar denoting the stepsize
+
+    Returns:
+        losses: a list of length max_iters containing the loss value (scalar) for each iteration of GD
+        ws: a list of length max_iters + 1 containing the model parameters as numpy arrays of shape (2, ),
+            for each iteration of GD (as well as the final weights)
+    """
+    # Define parameters to store w and loss
+    ws = [initial_w]
+    losses = []
+    w = initial_w
+    print(gamma)
+    for n_iter in range(max_iters):
+        # ***************************************************
+        # INSERT YOUR CODE HERE
+        # TODO: compute gradient and loss
+        # ***************************************************
+        grad = compute_gradient(y, tx, w)
+        loss = compute_loss(y, tx, w)
+        # ***************************************************
+        # INSERT YOUR CODE HERE
+        # TODO: update w by gradient
+        # ***************************************************
+        w = w - np.dot(gamma,grad)
+        
+        # store w and loss
+        ws.append(w)
+        losses.append(loss)
+        print(
+            "GD iter. {bi}/{ti}: loss={l}, w0={w0}, w1={w1}".format(
+                bi=n_iter, ti=max_iters - 1, l=loss, w0=w[0], w1=w[1]
+            )
+        )
+
+    return losses, ws
+
+
+def compute_subgradient_mae(y, tx, w):
+    """Compute a subgradient of the MAE at w.
+
+    Args:
+        y: numpy array of shape=(N, )
+        tx: numpy array of shape=(N,2)
+        w: numpy array of shape=(2, ). The vector of model parameters.
+
+    Returns:
+        A numpy array of shape (2, ) (same shape as w), containing the subgradient of the MAE at w.
+    """
+    # ***************************************************
+    # INSERT YOUR CODE HERE
+    # TODO: compute subgradient gradient vector for MAE
+    # ***************************************************
+    e = y - tx.dot(w)
+    n = y.shape[0]
+    s = np.sign(e)
+    return -1/n * tx.T.dot(s) 
+
+
+def mean_squared_error_sgd(y, tx, initial_w, max_iters, gamma):
+    """The SubGradient Descent (SubGD) algorithm.
+
+    Args:
+        y: numpy array of shape=(N, )
+        tx: numpy array of shape=(N,2)
+        initial_w: numpy array of shape=(2, ). The initial guess (or the initialization) for the model parameters
+        max_iters: a scalar denoting the total number of iterations of GD
+        gamma: a scalar denoting the stepsize
+
+    Returns:
+        losses: a list of length max_iters containing the loss value (scalar) for each iteration of SubGD
+        ws: a list of length max_iters containing the model parameters as numpy arrays of shape (2, ), for each iteration of SubGD
+    """
+    # Define parameters to store w and loss
+    ws = [initial_w]
+    losses = []
+    w = initial_w
+    for n_iter in range(max_iters):
+        # ***************************************************
+        # INSERT YOUR CODE HERE
+        # TODO: compute subgradient and loss
+        # ***************************************************
+        sub = compute_subgradient_mae(y, tx, w)
+        loss = compute_loss(y, tx, w)
+        # ***************************************************
+        # INSERT YOUR CODE HERE
+        # TODO: update w by subgradient
+        # ***************************************************
+        w = w- np.dot(gamma,sub)
+        ws.append(w)
+        losses.append(loss)
+        print(
+            "SubGD iter. {bi}/{ti}: loss={l}, w0={w0}, w1={w1}".format(
+                bi=n_iter, ti=max_iters - 1, l=loss, w0=w[0], w1=w[1]
+            )
+        )
+
+    return losses, ws
+
+
+
+def least_squares(y, tx):
+    """Calculate the least squares solution.
+       returns mse, and optimal weights.
+
+    Args:
+        y: numpy array of shape (N,), N is the number of samples.
+        tx: numpy array of shape (N,D), D is the number of features.
+
+    Returns:
+        w: optimal weights, numpy array of shape(D,), D is the number of features.
+        mse: scalar.
+
+    >>> least_squares(np.array([0.1,0.2]), np.array([[2.3, 3.2], [1., 0.1]]))
+    (array([ 0.21212121, -0.12121212]), 8.666684749742561e-33)
+    """
+    # ***************************************************
+    # INSERT YOUR CODE HERE
+    # least squares: TODO
+    # returns mse, and optimal weights
+    # ***************************************************
+    val = np.linalg.inv(np.dot(np.transpose(tx),tx))
+    
+    wa =np.dot(val,np.transpose(tx))
+    w = np.dot(wa,y)
+    mse = compute_loss(y, tx, w)
+    return w, mse
+
+
+def ridge_regression(y, tx, lambda_):
+    """implement ridge regression.
+
+    Args:
+        y: numpy array of shape (N,), N is the number of samples.
+        tx: numpy array of shape (N,D), D is the number of features.
+        lambda_: scalar.
+
+    Returns:
+        w: optimal weights, numpy array of shape(D,), D is the number of features.
+
+    >>> ridge_regression(np.array([0.1,0.2]), np.array([[2.3, 3.2], [1., 0.1]]), 0)
+    array([ 0.21212121, -0.12121212])
+    >>> ridge_regression(np.array([0.1,0.2]), np.array([[2.3, 3.2], [1., 0.1]]), 1)
+    array([0.03947092, 0.00319628])
+    """
+    N,D = tx.shape
+    w = np.dot(np.linalg.inv(tx.T @ tx + 2*N*lambda_*np.eye(D)),tx.T @ y)
+    return w
